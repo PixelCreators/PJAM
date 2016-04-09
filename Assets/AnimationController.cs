b0dea6b70962153c0f82e173157ef91dd11b4770
Mateusz Pusty <mateusz.pusty@pixelcreators.pl>
2016-04-09 17:36:14 +0200

    Added wake up animation

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour {
+
+    public void Switch()
+    {
+        FindObjectOfType<SwitchToPlayer>().Switch();
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/SwitchToPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class SwitchToPlayer : MonoBehaviour
+{
+    public Vector3 SpawnPoint;
+    public Vector3 Rotation;
+    public GameObject Player;
+
+    public void Switch()
+    {
+        Instantiate(Player, SpawnPoint, Quaternion.Euler(Rotation));
+    }
+}
