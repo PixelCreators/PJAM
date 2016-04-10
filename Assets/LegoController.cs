85e8338350f662285daaaadcfbc31a2149ffbd4d
Mateusz Pusty <mateusz.pusty@pixelcreators.pl>
2016-04-10 04:12:55 +0200

    Added 4 Level

---
+++ Assets/LegoController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LegoController : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            var anim = other.GetComponent<AnimationController>();
+            anim._cutSceneController.gameObject.SetActive(true);
+            anim.PlayDie();
+        }
+    }
+}
